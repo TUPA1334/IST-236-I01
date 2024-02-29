8c8aa1e8a6d95bc508b531e5409863b357d0d833
Brandon Tupa <tupa1334@gmail.com>
2024-02-28 20:09:48 -0500

    Continue BJ app

---
+++ git repos/Week5/BlackJackTheGame/constants/cards.js
@@ -0,0 +1,68 @@
+const Cards = {
+  twoClubs: require("../assets/images/cards/2_of_clubs.png"),
+  twoDiamonds: require("../assets/images/cards/2_of_diamonds.png"),
+  twoHearts: require("../assets/images/cards/2_of_hearts.png"),
+  twoSpades: require("../assets/images/cards/2_of_spades.png"),
+
+  threeClubs: require("../assets/images/cards/3_of_clubs.png"),
+  threeDiamonds: require("../assets/images/cards/3_of_diamonds.png"),
+  threeHearts: require("../assets/images/cards/3_of_hearts.png"),
+  threeSpades: require("../assets/images/cards/3_of_spades.png"),
+
+  fourClubs: require("../assets/images/cards/4_of_clubs.png"),
+  fourDiamonds: require("../assets/images/cards/4_of_diamonds.png"),
+  fourHearts: require("../assets/images/cards/4_of_hearts.png"),
+  fourSpades: require("../assets/images/cards/4_of_spades.png"),
+
+  fiveClubs: require("../assets/images/cards/5_of_clubs.png"),
+  fiveDiamonds: require("../assets/images/cards/5_of_diamonds.png"),
+  fiveHearts: require("../assets/images/cards/5_of_hearts.png"),
+  fiveSpades: require("../assets/images/cards/5_of_spades.png"),
+
+  sixClubs: require("../assets/images/cards/6_of_clubs.png"),
+  sixDiamonds: require("../assets/images/cards/6_of_diamonds.png"),
+  sixHearts: require("../assets/images/cards/6_of_hearts.png"),
+  sixSpades: require("../assets/images/cards/6_of_spades.png"),
+
+  sevenClubs: require("../assets/images/cards/7_of_clubs.png"),
+  sevenDiamonds: require("../assets/images/cards/7_of_diamonds.png"),
+  sevenHearts: require("../assets/images/cards/7_of_hearts.png"),
+  sevenSpades: require("../assets/images/cards/7_of_spades.png"),
+
+  eightClubs: require("../assets/images/cards/8_of_clubs.png"),
+  eightDiamonds: require("../assets/images/cards/8_of_diamonds.png"),
+  eightHearts: require("../assets/images/cards/8_of_hearts.png"),
+  eightSpades: require("../assets/images/cards/8_of_spades.png"),
+
+  nineClubs: require("../assets/images/cards/9_of_clubs.png"),
+  nineDiamonds: require("../assets/images/cards/9_of_diamonds.png"),
+  nineHearts: require("../assets/images/cards/9_of_hearts.png"),
+  nineSpades: require("../assets/images/cards/9_of_spades.png"),
+
+  tenClubs: require("../assets/images/cards/10_of_clubs.png"),
+  tenDiamonds: require("../assets/images/cards/10_of_diamonds.png"),
+  tenHearts: require("../assets/images/cards/10_of_hearts.png"),
+  tenSpades: require("../assets/images/cards/10_of_spades.png"),
+
+  jackClubs: require("../assets/images/cards/jack_of_clubs.png"),
+  jackDiamonds: require("../assets/images/cards/jack_of_diamonds.png"),
+  jackHearts: require("../assets/images/cards/jack_of_hearts.png"),
+  jackSpades: require("../assets/images/cards/jack_of_spades.png"),
+
+  queenClubs: require("../assets/images/cards/queen_of_clubs.png"),
+  queenDiamonds: require("../assets/images/cards/queen_of_diamonds.png"),
+  queenHearts: require("../assets/images/cards/queen_of_hearts.png"),
+  queenSpades: require("../assets/images/cards/queen_of_spades.png"),
+
+  kingClubs: require("../assets/images/cards/king_of_clubs.png"),
+  kingDiamonds: require("../assets/images/cards/king_of_diamonds.png"),
+  kingHearts: require("../assets/images/cards/king_of_hearts.png"),
+  kingSpades: require("../assets/images/cards/king_of_spades.png"),
+
+  aceClubs: require("../assets/images/cards/ace_of_clubs.png"),
+  aceDiamonds: require("../assets/images/cards/ace_of_diamonds.png"),
+  aceHearts: require("../assets/images/cards/ace_of_hearts.png"),
+  aceSpades: require("../assets/images/cards/ace_of_spades.png"),
+};
+
+export default Cards;
