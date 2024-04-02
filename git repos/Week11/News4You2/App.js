296b041bfa31274f553abbd45d2688e550e05d06
Brandon Tupa <tupa1334@gmail.com>
2024-04-02 10:14:27 -0400

    Start of News4You2

---
+++ git repos/Week11/News4You2/App.js
@@ -0,0 +1,177 @@
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
+import { useFonts } from "expo-font";
+import * as SplashScreen from "expo-splash-screen";
+import { NavigationContainer } from "@react-navigation/native";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import { createDrawerNavigator } from "@react-navigation/drawer";
+import BookmarksScreen from "./screens/BookmarksScreen";
+import NewsDetailsScreen from "./screens/NewsDetailsScreen";
+import CombatSportsNewsScreen from "./screens/CombatSportsNewsScreen";
+import USNewsScreen from "./screens/USNewsScreen";
+import WorldNewsScreen from "./screens/WorldNewsScreen";
+import Colors from "./constants/colors";
+import {
+  Entypo,
+  MaterialIcons,
+  MaterialCommunityIcons,
+} from "@expo/vector-icons";
+import { useCallback } from "react";
+
+const Stack = createNativeStackNavigator();
+const Drawer = createDrawerNavigator();
+const Tabs = createBottomTabNavigator();
+
+function DrawerNavigator() {
+  return (
+    <Drawer.Navigator
+      initialRouteName="Listings"
+      screenOptions={{
+        headerStyle: { backgroundColor: Colors.primary500 },
+        headerTintColor: "white",
+        headerTitleStyle: {
+          fontFamily: "playfair",
+          fontSize: 40,
+          color: Colors.accent800,
+        },
+        sceneContainerStyle: { backgroundColor: Colors.primary300 },
+        drawerContentStyle: { backgroundColor: Colors.primary500 },
+        drawerInactiveTintColor: Colors.primary300,
+        drawerActiveTintColor: Colors.accent500,
+        drawerActiveBackgroundColor: Colors.primary800,
+      }}
+    >
+      <Drawer.Screen
+        name="Listings"
+        component={TabsNavigator}
+        options={{
+          title: "Todays News",
+          drawerLabel: "Top News",
+          drawerIcon: ({ color, size }) => (
+            <Entypo name="list" size={size} color={color} />
+          ),
+        }}
+      />
+      <Drawer.Screen
+        name="BookmarkedListings"
+        component={BookmarksScreen}
+        options={{
+          title: "Saved Articles",
+          drawerLabel: "Saved Articles",
+          drawerIcon: ({ color, size }) => (
+            <Entypo name="bookmark" size={size} color={color} />
+          ),
+        }}
+      />
+    </Drawer.Navigator>
+  );
+}
+
+function TabsNavigator() {
+  return (
+    <Tabs.Navigator
+      screenOptions={{
+        tabBarShowLabel: true,
+        tabBarActiveTintColor: Colors.accent500,
+        tabBarInactiveTintColor: Colors.primary300,
+        tabBarActiveBackgroundColor: Colors.primary800,
+        tabBarInactiveBackgroundColor: Colors.primary500,
+        tabBarLabelStyle: {
+          fontFamily: "playfair",
+          fontSize: 12,
+        },
+      }}
+    >
+      <Tabs.Screen
+        name="HouseListings"
+        component={USNewsScreen}
+        options={{
+          headerShown: false,
+          tabBarIcon: ({ color, size }) => (
+            <Entypo name="news" size={size} color={color} />
+          ),
+          tabBarLabel: "US News",
+        }}
+      />
+      <Tabs.Screen
+        name="CondoListings"
+        component={WorldNewsScreen}
+        options={{
+          headerShown: false,
+          tabBarIcon: ({ color, size }) => (
+            <Entypo name="megaphone" size={size} color={color} />
+          ),
+          tabBarLabel: "World News",
+        }}
+      />
+      <Tabs.Screen
+        name="CombatSportsNews"
+        component={CombatSportsNewsScreen}
+        options={{
+          headerShown: false,
+          tabBarIcon: ({ color, size }) => (
+            <Entypo name="medal" size={size} color={color} />
+          ),
+          tabBarLabel: "Combat Sports",
+        }}
+      />
+    </Tabs.Navigator>
+  );
+}
+
+export default function App() {
+  const [fontsLoaded, fontError] = useFonts({
+    playfair: require("./assets/fonts/CherryCreamSoda-Regular.ttf"),
+  });
+
+  const onLayoutRootView = useCallback(async () => {
+    if ((fontsLoaded, fontError)) {
+      await SplashScreen.hideAsync();
+    }
+  });
+
+  if (!fontsLoaded && !fontError) {
+    return null;
+  } else {
+    return (
+      <>
+        <StatusBar style="auto" />
+        <NavigationContainer>
+          <Stack.Navigator
+            initialRouteName="DrawerScreen"
+            screenOptions={{
+              headerTintColor: Colors.primary300,
+              headerStyle: { backgroundColor: Colors.primary500 },
+              contentStyle: { backgroundColor: "black" },
+            }}
+          >
+            <Stack.Screen
+              name="DrawerScreen"
+              component={DrawerNavigator}
+              options={{
+                headerShown: false,
+              }}
+            />
+            <Stack.Screen
+              name="NewsDetailsScreen"
+              component={NewsDetailsScreen}
+              options={{
+                headerBackTitleVisible: false,
+              }}
+            />
+          </Stack.Navigator>
+        </NavigationContainer>
+      </>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+});
---
+++ git repos/Week11/News4You2/constants/colors.js
@@ -0,0 +1,12 @@
+const Colors = {
+  accent200: "#F39c12",
+  accent500: "#E74c3c",
+  accent800: "#ECF0F1",
+  primary300o5: "rgba(255, 255, 255, 0.5)",
+  primary300: "#FFFFFF",
+  primary500o8: "rgba(52, 73, 94, 0.8)",
+  primary500: "#34495E",
+  primary800: "#2C3E50",
+};
+
+export default Colors;
